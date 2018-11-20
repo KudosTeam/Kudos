2175d7b3e00fc3144b0547db1f946ac33f4990e8
egurinko <egctoru@gmail.com>
2018-11-20 20:50:02 +0900

    Add app.json file for making review app in heroku.

--- db/index.js
+++ db/index.js
@@ -4,7 +4,7 @@ const knex = Knex({
   client: "pg",
   connection:
     process.env.DATABASE_URL ||
-    `postgres://${process.env.USER}@127.0.0.1:5432/love_app`,
+    `postgres://${process.env.USER}@127.0.0.1:5432/kudos_app`,
   searchPath: "public"
 });
 
