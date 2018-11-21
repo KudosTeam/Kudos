91877cf112a8dcc0450ea95e0af6e5de781983b3
Sakura <sorahonda2@gmail.com>
2018-11-21 16:02:31 +0900

    added logo

--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -2,25 +2,14 @@ import React, { Component } from "react";
 import { SelectField, MenuItem } from "material-ui";
 
 class Form extends Component {
-  // onChange = e => {
-  //   // this.setState({ [e.target.name]: e.target.value });
-  // };
-  componentWillMount() {
-    // this.props.fetchCompliments();
-  }
   render() {
-    console.log(this.props);
-    // const complimentsPullDown = this.props.compliments.compliments.map(
-    //   compliment => (
-    //     <option key={compliment.id} value={compliment.text}>
-    //       {compliment.text}
-    //     </option>
-    //   )
-    // );
-
     const complimentsPullDown = [
-      {payload: '0', text: 'Yo momma so poor that ducks throw bread at her'},
-      {payload: '1', text: 'Yo momma so dumb that she went to an L.A. Clippers game to get a hair cut'}
+      { payload: "0", text: "Yo momma so poor that ducks throw bread at her" },
+      {
+        payload: "1",
+        text:
+          "Yo momma so dumb that she went to an L.A. Clippers game to get a hair cut"
+      }
     ];
 
     return (
@@ -30,19 +19,30 @@ class Form extends Component {
           <div>
             <label>Compliment:</label>
             <br />
-            <SelectField name="compliment" onChange={this.props.storeCompliment} menuItems={complimentsPullDown}>
-            <MenuItem payload='0' value='Yo mama so poor that ducks throw bread at her'>Yo mama so poor that ducks throw bread at her</MenuItem>
-   <MenuItem payload='1' value='Yo momma so dumb that she went to an L.A. Clippers game to get a hair cut'>Yo momma so dumb that she went to an L.A. Clippers game to get a hair cut</MenuItem>
+            <SelectField
+              name="compliment"
+              onChange={this.props.storeCompliment}
+              menuItems={complimentsPullDown}
+            >
+              <MenuItem
+                payload="0"
+                value="Yo mama so poor that ducks throw bread at her"
+              >
+                Yo mama so poor that ducks throw bread at her
+              </MenuItem>
+              <MenuItem
+                payload="1"
+                value="Yo momma so dumb that she went to an L.A. Clippers game to get a hair cut"
+              >
+                Yo momma so dumb that she went to an L.A. Clippers game to get a
+                hair cut
+              </MenuItem>
             </SelectField>
           </div>
           <div>
             <label>Phone Number:</label>
             <br />
-            <input
-              type="text"
-              name="phone"
-              onChange={this.props.storePhone}
-            />
+            <input type="text" name="phone" onChange={this.props.storePhone} />
           </div>
           <br />
           <button onClick={this.props.makeCall}>Send Compliment</button>
