0428b7f971732e33ec8fbd83cb607283973a490e
chcliu <liucharlesche@gmail.com>
2018-11-22 20:00:11 +0900

    Had to fix knexfile

--- db/knexfile.js
+++ db/knexfile.js
@@ -1,12 +1,12 @@
 module.exports = {
   client: "postgresql",
-  // connection:
-  //   process.env.DATABASE_URL ||
-  //   `postgres://${process.env.USER}@127.0.0.1:5432/kudos_app`,
-  connection: {
-    database: "compliments",
-    user: "postgres",
-  },
+  connection:
+    process.env.DATABASE_URL ||
+    `postgres://${process.env.USER}@127.0.0.1:5432/kudos_app`,
+  // connection: {
+  //   database: "compliments",
+  //   user: "postgres",
+  // },
   pool: {
     min: 2,
     max: 10
