11368cb30838604c455f5c82d8ce424d421c88b6
Sakura <sorahonda2@gmail.com>
2018-11-22 23:31:03 +0900

    added visual improvement for phone input

--- src/components/Phone.jsx
+++ src/components/Phone.jsx
@@ -1,27 +1,34 @@
 import React, { Component } from "react";
-import { RaisedButton, TextField } from "material-ui";
-import { Typography } from '@material-ui/core';
-import 'typeface-roboto';
+import { RaisedButton } from "material-ui";
+import { Typography, TextField } from "@material-ui/core";
+import "typeface-roboto";
 
 import { SchedulerContainer } from "../containers/Scheduler";
 
 class Phone extends Component {
-    render() {
-        const props = this.props;
-        return (
-            <div className="phone">
-                <Typography variant="body1">Phone Number</Typography>
-                <br />
-                <TextField type="text" id="phone" onChange={props.storePhone} />
-                <br />
-                <SchedulerContainer />
-                <RaisedButton
-                    label="Send Compliment"
-                    onClick={() => props.makeCall(props.isCalled)}
-                />
-            </div>
-        );
-    }
+  render() {
+    const props = this.props;
+    return (
+      <div className="phone">
+        <Typography variant="body1" />
+        <br />
+        <TextField
+          type="text"
+          id="phone"
+          label="phone number"
+          placeholder="81 90 0000 0000"
+          style={{ margin: 8 }}
+          onChange={props.storePhone}
+        />
+        <br />
+        <SchedulerContainer />
+        <RaisedButton
+          label="Send Compliment"
+          onClick={() => props.makeCall(props.isCalled)}
+        />
+      </div>
+    );
+  }
 }
 
 export default Phone;
