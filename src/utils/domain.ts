9355f208d10694a615803b40f3dc26191c701e4a
egurinko <egctoru@gmail.com>
2020-01-04 14:14:42 +0900

    change domain to api

--- src/utils/domain.ts
+++ src/utils/domain.ts
@@ -1,5 +1,5 @@
 export default window.location.href.includes("localhost")
   ? `http://localhost:4000/api`
   : window.location.href.includes("stg-kudos-love")
-  ? "https://stg-kudos-love.herokuapp.com/"
-  : "https://kudos-love.herokuapp.com/";
+  ? "https://stg-kudos-love.herokuapp.com/api"
+  : "https://kudos-love.herokuapp.com/api";
